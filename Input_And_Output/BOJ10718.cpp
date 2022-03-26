d8e07aada3780c4e07bc389c37091b2a7a84214a
Hirari <lsw9281@naver.com>
2022-03-26 11:50:11 +0900

    220326_We love kriii
    10718

---
+++ Input_And_Output/BOJ10718.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+// using namespace std;
+
+int main()
+{
+    std::cout << "강한친구 대한육군" << std::endl;
+    std::cout << "강한친구 대한육군" << std::endl;
+    return 0;
+}
