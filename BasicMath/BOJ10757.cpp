6e568b63f0cb3169caa8f45276cc12055846bae7
Hirari <lsw9281@naver.com>
2023-01-10 21:23:14 +0900

    Update 230110
    1316해결

---
+++ BasicMath/BOJ10757.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+int main()
+{   
+    std::string str1, str2, answer;
+    std::cin >> str1 >> str2;
+
+    int str1len = str1.length();
+    int str2len = str2.length();
+
+    char arr[10000] = {0,};
+
+    for(int i = 0; i<str1len && i<str2len; i++)
+    {
+        std::cout << "t";
+        int carry = 0;
+        int sum = 0;
+
+        sum = (int)str1[i]-48 + (int)str2[i]-48 + arr[i];
+
+        if(sum >= 10)
+        {
+            sum -= 10;
+            carry = 1;
+        }
+
+        arr[i] = sum;
+        arr[i+1] = carry;
+    }
+
+    for(int i = 0; i<str1len && i<str2len; i++)
+    {
+        std::cout << arr[i] << "x";
+        answer.push_back(arr[i]);
+    }
+
+    std::cout << answer;
+
+    return 0;
+}
--- String/BOJ1316.cpp
+++ String/BOJ1316.cpp
@@ -3,9 +3,39 @@
 
 int main()
 {   
-    std::string str;
-    std::cin >> str;
+    int N;
+    int Answer = 0;
+    std::cin >> N;  
 
-    
+    while(N--)
+    {
+        bool isAnswerUp = true;
+        int index = 0;
+        int alpa[26] = {0,};
+        std::string str;
+        std::cin >> str;
+
+        for(int length = str.length(); length > index; index++)
+        {
+            if(alpa[(int)str[index]-97] >= 1)
+            {
+                isAnswerUp = false;
+                break;
+            }
+            alpa[(int)str[index]-97]++;
+            for(int i = 1; str[index] == str[index + i];)
+            {
+                index++;
+            }
+        }
+  
+        if(isAnswerUp)
+        {
+            Answer++;
+        }
+    }
+
+    std::cout << Answer;
+
     return 0;
 }
